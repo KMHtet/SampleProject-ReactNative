e880c3b8b0f9c21bf2d14ad254c350c990c3e48c
Kyaw Myo Htet <kyawmyohtet@mytel.com.mm>
2021-09-10 22:08:59 +0630

    Add Themes

---
+++ src/themes/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    //ACTIONS
+    success: '#3adb76',
+    warning: '#ffae00',
+    error: '#cc4b37',
+
+    //COLORS
+    white: '#FFFFFF',
+    black: '#000000',
+};
---
+++ src/themes/fonts.js
@@ -0,0 +1,37 @@
+import Metrics from './metrics';
+
+export const fontSizes = {
+    font6: Metrics.screenWidth * (6 / 365),
+    font7: Metrics.screenWidth * (7 / 365),
+    font8: Metrics.screenWidth * (8 / 365),
+    font9: Metrics.screenWidth * (9 / 365),
+    font10: Metrics.screenWidth * (10 / 365),
+    font11: Metrics.screenWidth * (11 / 365),
+    font12: Metrics.screenWidth * (12 / 365),
+    font13: Metrics.screenWidth * (13 / 365),
+    font14: Metrics.screenWidth * (14 / 365),
+    font15: Metrics.screenWidth * (15 / 365),
+    font16: Metrics.screenWidth * (16 / 365),
+    font17: Metrics.screenWidth * (17 / 365),
+    font18: Metrics.screenWidth * (18 / 365),
+    font19: Metrics.screenWidth * (19 / 365),
+    font20: Metrics.screenWidth * (20 / 365),
+};
+
+export const fontWeight = {
+    full: '900',
+    semi: '600',
+    low: '400',
+    bold: 'bold',
+    normal: 'normal',
+};
+
+export const fontFamily = {
+    medium: '',
+    regular: '',
+    semiBold: '',
+};
+
+export const fonts = {
+
+};
