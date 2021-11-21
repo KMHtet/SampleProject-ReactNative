d0e70458904a0bbd53f99f5bd8c733be020646ac
Kyaw Myo Htet <kyawmyohtet@mytel.com.mm>
2021-11-21 18:21:30 +0630

    Initial App

---
+++ src/containers/SampleScreen2/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class SampleScreen1 extends React.PureComponent{
+    render() {
+        return (
+            <View>
+                <Text>Hello World 2.</Text>
+            </View>
+        )
+    }
+}
---
+++ src/locales/index.js
@@ -0,0 +1,7 @@
+import eng from './english/english_translation';
+import my_uni from './myanmar/myanmar_unicode_translation';
+
+export default{
+    eng,
+    my_uni,
+};
